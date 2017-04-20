056d5c731cc54ea99d0a07c631a615fbfc51b37b
bung87 <zh.bung@gmail.com>
2017-04-20 23:25:28 +0800

    o

--- follow/urls.py
+++ follow/urls.py
@@ -2,15 +2,15 @@ try:
     from django.conf.urls import url, include
 except ImportError:
     from django.conf.urls.defaults import *
-
+from . import views
 urlpatterns = (
-    url(r'^toggle/(?P<app>[^\/]+)/(?P<model>[^\/]+)/(?P<id>\d+)/$', 'follow.views.toggle', name='toggle'),
-    url(r'^toggle/(?P<app>[^\/]+)/(?P<model>[^\/]+)/(?P<id>\d+)/$', 'follow.views.toggle', name='follow'),
-    url(r'^toggle/(?P<app>[^\/]+)/(?P<model>[^\/]+)/(?P<id>\d+)/$', 'follow.views.toggle', name='unfollow'),
-    url(r'^vendorfollowers/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$', 'follow.views.get_vendor_followers', name='get_vendor_followers'),
-	url(r'^vendorfollowing/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$', 'follow.views.get_vendor_following', name='get_vendor_following'),
+    url(r'^toggle/(?P<app>[^\/]+)/(?P<model>[^\/]+)/(?P<id>\d+)/$', views.toggle, name='toggle'),
+    url(r'^toggle/(?P<app>[^\/]+)/(?P<model>[^\/]+)/(?P<id>\d+)/$', views.toggle, name='follow'),
+    url(r'^toggle/(?P<app>[^\/]+)/(?P<model>[^\/]+)/(?P<id>\d+)/$', views.toggle, name='unfollow'),
+    url(r'^vendorfollowers/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$', views.get_vendor_followers, name='get_vendor_followers'),
+	url(r'^vendorfollowing/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$', views.get_vendor_following, name='get_vendor_following'),
     url(r'^vendor_followers_subset/(?P<content_type_id>\d+)/(?P<object_id>\d+)/(?P<sIndex>\d+)/(?P<lIndex>\d+)/$',
-        'follow.views.get_vendor_followers_subset', name='get_vendor_followers_subset'),
+        views.get_vendor_followers_subset, name='get_vendor_followers_subset'),
     url(r'^vendor_following_subset/(?P<content_type_id>\d+)/(?P<object_id>\d+)/(?P<sIndex>\d+)/(?P<lIndex>\d+)/$',
-        'follow.views.get_vendor_following_subset', name='get_vendor_following_subset'),
+        views.get_vendor_following_subset, name='get_vendor_following_subset'),
 )
