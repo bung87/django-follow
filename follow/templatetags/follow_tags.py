3000cdf5b7bb8a039db9234adb6c298c7b1c5fd1
bung87 <zh.bung@gmail.com>
2017-04-24 18:08:47 +0800

    o

--- follow/templatetags/follow_tags.py
+++ follow/templatetags/follow_tags.py
@@ -84,8 +84,8 @@ class FollowFormNode(template.Node):
         self.template = tpl[1:-1] if tpl else 'follow/form.html'
     def render(self, context):
         request = template.Variable("request").resolve(context)
-        ctx = {'object': self.obj.resolve(context),'request':request,'csrf_token':get_token(request)}
-        return template.loader.render_to_string(self.template, ctx)
+        ctx = {'object': self.obj.resolve(context)}
+        return template.loader.render_to_string(self.template, ctx,request = request)
 
 class FollowingList(template.Node):
     def __init__(self, obj):
